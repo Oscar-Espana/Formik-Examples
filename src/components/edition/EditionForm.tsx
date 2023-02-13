302c5034325515141226949417508c0179b76ec7
Oscar Espana <espaaoscar@gmail.com>
2023-02-12 19:21:30 -0500

    recycle: building the EditionForm to refactor the edicion page

---
+++ src/components/edition/EditionForm.tsx
@@ -0,0 +1,92 @@
+import { countries, activities, cities } from "@/constants";
+import { FormData } from "@/pages/edicion";
+import { Box, Typography, Button } from "@mui/material";
+import React from "react";
+import { Autocomplete, TextInput } from "../ui";
+
+interface Props {
+  handleSubmit: (e?: React.FormEvent<HTMLFormElement> | undefined) => void;
+  setFieldValue: (
+    field: string,
+    value: any,
+    shouldValidate?: boolean | undefined
+  ) => void;
+  values: FormData;
+}
+export const EditionForm = ({ handleSubmit, setFieldValue, values }: Props) => {
+  const getCitiesFilteredByCountry = (countryId: number | undefined) => {
+    return cities.filter((city) => city.countryId === countryId);
+  };
+  return (
+    <Box
+      component={"form"}
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        width: "100%",
+        gap: 2.5,
+      }}
+      onSubmit={handleSubmit}
+    >
+      <Typography
+        textAlign={"center"}
+        component="h2"
+        sx={{
+          fontWeight: "500",
+          fontSize: "28px",
+          mb: 1.5,
+        }}
+      >
+        Planificar viaje
+      </Typography>
+      <Autocomplete
+        label="País"
+        name="country"
+        options={countries}
+        getOptionLabel={(option) => option.name ?? option}
+        onChange={(value) => {
+          setFieldValue("country", value);
+          setFieldValue("city", null);
+        }}
+      />
+
+      <Autocomplete
+        label="Ciudad"
+        name="city"
+        options={getCitiesFilteredByCountry(values.country?.id)}
+        getOptionLabel={(option) => option.name ?? option}
+      />
+
+      <TextInput type="number" name="budget" label="Presupuesto estimado" />
+
+      <Typography>Actividades a realizar:</Typography>
+      <Box sx={{ display: "flex", justifyContent: "space-around" }}>
+        {activities.map((activity) => (
+          <Box key={activity.id}>
+            <Box
+              sx={{
+                width: "50px",
+                height: "50px",
+                borderRadius: "50%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                border: "solid 1px gray",
+                cursor: "pointer",
+              }}
+            >
+              {activity.icon}
+            </Box>
+            <Typography variant="caption" textAlign="center">
+              {activity.label}
+            </Typography>
+          </Box>
+        ))}
+      </Box>
+
+      <Button variant="contained" sx={{ mt: 2 }} type="submit">
+        Registrar
+      </Button>
+    </Box>
+  );
+};
--- src/pages/edicion.tsx
+++ src/pages/edicion.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Box, Button, Container, Grid, Typography } from "@mui/material";
+import { Box, Container, Grid } from "@mui/material";
 import { Formik } from "formik";
-import { cities, countries } from "@/constants";
 import { ICity, ICountry } from "@/interfaces";
-import { Autocomplete, TextInput } from "@/components/ui";
-import { activities } from "../constants/activities";
+import { EditionForm } from "@/components/edition";
 
-interface FormData {
+export interface FormData {
   country: ICountry | null;
   city: ICity | null;
   budget: number;
@@ -20,9 +18,6 @@ const initialValues: FormData = {
   activities: [],
 };
 const EditionPage = () => {
-  const getCitiesFilteredByCountry = (countryId: number | undefined) => {
-    return cities.filter((city) => city.countryId === countryId);
-  };
   return (
     <Box>
       <Container>
@@ -35,80 +30,11 @@ const EditionPage = () => {
               }}
             >
               {({ values, setFieldValue, handleSubmit }) => (
-                <Box
-                  component={"form"}
-                  sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    width: "100%",
-                    gap: 2.5,
-                  }}
-                  onSubmit={handleSubmit}
-                >
-                  <Typography
-                    textAlign={"center"}
-                    component="h2"
-                    sx={{
-                      fontWeight: "500",
-                      fontSize: "28px",
-                      mb: 1.5,
-                    }}
-                  >
-                    Planificar viaje
-                  </Typography>
-                  <Autocomplete
-                    label="País"
-                    name="country"
-                    options={countries}
-                    getOptionLabel={(option) => option.name ?? option}
-                    onChange={(value) => {
-                      setFieldValue("country", value);
-                      setFieldValue("city", null);
-                    }}
-                  />
-
-                  <Autocomplete
-                    label="Ciudad"
-                    name="city"
-                    options={getCitiesFilteredByCountry(values.country?.id)}
-                    getOptionLabel={(option) => option.name ?? option}
-                  />
-
-                  <TextInput
-                    type="number"
-                    name="budget"
-                    label="Presupuesto estimado"
-                  />
-
-                  <Typography>Actividades a realizar:</Typography>
-                  <Box sx={{ display: "flex", justifyContent: "space-around" }}>
-                    {activities.map((activity) => (
-                      <Box key={activity.id}>
-                        <Box
-                          sx={{
-                            width: "50px",
-                            height: "50px",
-                            borderRadius: "50%",
-                            display: "flex",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            border: "solid 1px gray",
-                            cursor: "pointer",
-                          }}
-                        >
-                          {activity.icon}
-                        </Box>
-                        <Typography variant="caption" textAlign="center">
-                          {activity.label}
-                        </Typography>
-                      </Box>
-                    ))}
-                  </Box>
-
-                  <Button variant="contained" sx={{ mt: 2 }} type="submit">
-                    Registrar
-                  </Button>
-                </Box>
+                <EditionForm
+                  values={values}
+                  setFieldValue={setFieldValue}
+                  handleSubmit={handleSubmit}
+                />
               )}
             </Formik>
           </Grid>
